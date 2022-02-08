9d896cdd1739715981fe17f587b19afa86e750fd
Masud  Parvez <android@Masuds-Mac-mini.local>
2022-02-08 13:22:35 +0600

    working on payment

--- app/src/main/java/com/rockstreamer/rockstreamerpayment/FirstFragment.kt
+++ app/src/main/java/com/rockstreamer/rockstreamerpayment/FirstFragment.kt
@@ -6,39 +6,65 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.anjlab.android.iab.v3.BillingProcessor
+import com.anjlab.android.iab.v3.PurchaseInfo
 import com.rockstreamer.rockstreamerpayment.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment() , BillingProcessor.IBillingHandler{
 
     private var _binding: FragmentFirstBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
+
     private val binding get() = _binding!!
+
+    private lateinit var billingProcessor: BillingProcessor
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+
+        billingProcessor = BillingProcessor(requireActivity(), requireActivity().resources.getString(R.string.payment_token), this)
+
+        binding.buttonFirst.setOnClickListener {
+            billingProcessor.subscribe(requireActivity(), "funprime_gpinapp_weekly")
+        }
+
         return binding.root
 
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onProductPurchased(productId: String, details: PurchaseInfo?) {
+
+    }
+
+    override fun onPurchaseHistoryRestored() {
+
+    }
+
+    override fun onBillingError(errorCode: Int, error: Throwable?) {
+
+    }
+
+    override fun onBillingInitialized() {
+
     }
 }
